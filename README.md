7ba081d05dd39fa39a41702395727bf55b5040e0
Gagan <g.deepsingh1@gmail.com>
2025-06-22 21:00:48 -0600

    video support

--- README.md
+++ README.md
@@ -10,24 +10,47 @@ This library is designed to detect the lighting conditions from video stream or 
 npm install detect-camera-lighting
 ```
 
-## Usage
-
-Detecting lighting from an image:
+## Usage - Detect lighting from image
 
 ```javascript
 import DetectLighting from 'detect-camera-lighting';
 
 const detectLighting = new DetectLighting();
 
-const lightImageUrl = 'https://example.com/path/to/light.jpg';
+const lightImageUrl = './images/light.jpg';
 
-detectLighting.image(lightImageUrl)
-    .then(result => {
-        console.log(result); // output will be either 'light' or 'dark'
+const eventEmitter = detectLighting.image(lightImageUrl);
+eventEmitter.addEventListener('processed', (data) => {
+    console.log('Processed image:', data.detail);
+});
+
+```
+
+## Usage - Detect lighting from video stream
+
+Note: Ensure video is loaded before passing to the library
+
+```javascript
+import DetectLighting from 'detect-camera-lighting';
+
+navigator.mediaDevices.getUserMedia({ video: true })
+    .then((stream) => {
+        const video = document.getElementById('video');
+        video.srcObject = stream;
+
+        video.addEventListener('loadedmetadata', () => {
+            video.play();
+        });
+        video.addEventListener('play', () => {
+            detectLighting.video(video).addEventListener('processed', (data) => {
+                console.log('Processed video frame:', data.detail);
+            });
+        });
     })
-    .catch(error => {
-        console.error('Error detecting lighting:', error);
+    .catch((error) => {
+        console.error('Error accessing webcam:', error);
     });
 ```
+
 
 If you like my work, please consider giving it a star on [GitHub](http://github.com/gagan-bhullar-tech/detect-camera-lighting) or Sponsor my work by following [sponsoring me](https://github.com/sponsors/gagan-bhullar-tech) link.
--- src/index.ts
+++ src/index.ts
@@ -10,56 +10,74 @@ enum DetectLightingState {
 }
 
 export default class DetectLighting {
-    image(url: string, options?: Partial<IDetectLightingOptions>): Promise<string> {
-        return new Promise((resolve, reject) => {
-           try {
-                if (options && options.threshold !== undefined && (options.threshold < 0 || options.threshold > 1)) {
-                    throw new Error("'threshold' must be between 0 and 1.");
-                }
-                const threshold = options?.threshold || DEFAULT_THRESHOLD; // Default threshold value
-                const image = new Image();
-                image.onload = () => {
-                    this.processFrame(image, threshold)
-                        .then(result => resolve(result))
-                        .catch(err => reject(err));
-                };
-                image.onerror = (err) => {
-                    reject(new Error(`Failed to load image: ${err}`));
-                };
-                image.crossOrigin = "anonymous";
-                image.src = url;
-            } catch (error) {
-                return reject(error);
-           }
-        });
+    private eventTarget = new EventTarget();
+    lastVideoTime = 0;
+
+    image(url: string, options?: Partial<IDetectLightingOptions>): EventTarget {
+        if (options && options.threshold !== undefined && (options.threshold < 0 || options.threshold > 1)) {
+            throw new Error("'threshold' must be between 0 and 1.");
+        }
+        const threshold = options?.threshold || DEFAULT_THRESHOLD; // Default threshold value
+        const image = new Image();
+        const canvas = document.createElement('canvas');
+        image.onload = () => {
+            canvas.width = image.width;
+            canvas.height = image.height;
+            this.processFrame(image, canvas, threshold);
+        };
+        image.onerror = (err) => {
+            throw new Error(`Failed to load image: ${err}`);
+        };
+        image.crossOrigin = "anonymous";
+        image.src = url;
+        return this.eventTarget;
     }
 
-    private processFrame(frame: HTMLImageElement, threshold: number): Promise<string> {
-        return new Promise((resolve, reject) => {
-            const canvas = document.createElement('canvas');
-            const ctx = canvas.getContext('2d');
-            if (!ctx) {
-                return reject(new Error("Failed to get canvas context."));
-            }
-            canvas.width = frame.width;
-            canvas.height = frame.height;
-            ctx.drawImage(frame, 0, 0, frame.width, frame.height);
-            const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-            const data = imageData.data;
-            let totalBrightness = 0;
-            let pixelCount = 0;
-            for (let i = 0; i < data.length; i += 4) {
-                const r = data[i];
-                const g = data[i + 1];
-                const b = data[i + 2];
-                // Calculate brightness using the perceived brightness formula
-                const brightness = 0.299 * r + 0.587 * g + 0.114 * b;
-                totalBrightness += brightness;
-                pixelCount++;
-            }
-            const averageBrightness = totalBrightness / pixelCount;
-            const thresholdValue = 255 * threshold; // Convert threshold to a value between 0 and 255
-            return averageBrightness > thresholdValue ? resolve(DetectLightingState.LIGHT) : resolve(DetectLightingState.DARK);
-        });
+    video(video: HTMLVideoElement, options?: Partial<IDetectLightingOptions>): EventTarget {
+        if (options && options.threshold !== undefined && (options.threshold < 0 || options.threshold > 1)) {
+            throw new Error("'threshold' must be between 0 and 1.");
+        }
+        const threshold = options?.threshold || DEFAULT_THRESHOLD; // Default threshold value
+        if (!video) {
+            throw new Error("No video element provided.");
+        }
+        if (video.readyState < 2) {
+            throw new Error("Video is not ready. Ensure the video is loaded before processing.");
+        }
+        const canvas = document.createElement('canvas');
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        this.processFrame(video, canvas, threshold);
+        return this.eventTarget;
+    }
+
+    private processFrame(frame: HTMLImageElement | HTMLVideoElement, canvas: HTMLCanvasElement, threshold: number) {
+        const ctx = canvas.getContext('2d');
+        if (!ctx) {
+            throw new Error("Failed to get canvas context.");
+        }
+        ctx.drawImage(frame, 0, 0, canvas.width, canvas.height);
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const data = imageData.data;
+        let totalBrightness = 0;
+        let pixelCount = 0;
+        for (let i = 0; i < data.length; i += 4 * 10) {
+            const r = data[i];
+            const g = data[i + 1];
+            const b = data[i + 2];
+            // Calculate brightness using the perceived brightness formula
+            const brightness = 0.299 * r + 0.587 * g + 0.114 * b;
+            totalBrightness += brightness;
+            pixelCount++;
+        }
+        const averageBrightness = totalBrightness / pixelCount;
+        const thresholdValue = 255 * threshold; // Convert threshold to a value between 0 and 255
+        this.eventTarget.dispatchEvent(new CustomEvent("processed", { detail: averageBrightness > thresholdValue ? DetectLightingState.LIGHT : DetectLightingState.DARK }));
+        if (frame instanceof HTMLVideoElement) {
+            // If it's a video, request the next frame
+            requestAnimationFrame(() => {
+                this.processFrame(frame, canvas, threshold);
+            });
+        }
     }
 }
